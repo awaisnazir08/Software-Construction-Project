2d3aba83ce4028febf586d7059354a40867dd1f5
Faareh-Ahmed <faarehahmed@gmail.com>
2024-12-23 03:11:27 +0500

    Logg Status Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import {useAuth} from './contexts/AuthContext'
-
 export default function App() {
   const {isLoggedIn} = useAuth()
-
   return (
     <div className='App'>
       {/* <Header /> */}
@@ -11,13 +9,11 @@ export default function App() {
     </div>
   )
 }
-
 const LoggedInText = () => {
   const {account} = useAuth()
 
   return <p>Hey, {account.username}! I'm happy to let you know: you are authenticated!</p>
 }
-
 const LoggedOutText = () => (
   <p>Don't forget to start your backend server, then authenticate yourself.</p>
 )
